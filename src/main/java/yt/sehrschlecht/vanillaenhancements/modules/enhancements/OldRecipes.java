d7a8262072661150d8341f792949b82b97131316
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2021-12-23 22:01:15 +0100

    more old recipes

--- src/main/java/yt/sehrschlecht/vanillaenhancements/modules/enhancements/OldRecipes.java
+++ src/main/java/yt/sehrschlecht/vanillaenhancements/modules/enhancements/OldRecipes.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -17,6 +18,9 @@ public class OldRecipes extends VEModule {
     @Option public ConfigOption useIngotsForEnchantedGoldenApple = new ConfigOption("enchanted_golden_apple_use_ingots", getKey(), false);
     @Option public ConfigOption oldGoldenAppleRecipe = new ConfigOption("old_golden_apple_recipe", getKey(), false);
     @Option public ConfigOption oldChainmailArmorRecipe = new ConfigOption("old_chainmail_armor_recipe", getKey(), false);
+    @Option public ConfigOption horseArmorRecipes = new ConfigOption("horse_armor_recipes", getKey(), false);
+
+    private RecipeChoice.MaterialChoice woolChoice;
 
     @Override
     public @NotNull String getName() {
@@ -30,6 +34,12 @@ public class OldRecipes extends VEModule {
 
     @Override
     public void onEnable() {
+        woolChoice = new RecipeChoice.MaterialChoice(
+                Material.WHITE_WOOL, Material.ORANGE_WOOL, Material.MAGENTA_WOOL, Material.LIGHT_BLUE_WOOL, Material.YELLOW_WOOL, Material.LIME_WOOL,
+                Material.PINK_WOOL, Material.GRAY_WOOL, Material.LIGHT_GRAY_WOOL, Material.CYAN_WOOL, Material.PURPLE_WOOL, Material.BLUE_WOOL,
+                Material.BROWN_WOOL, Material.GREEN_WOOL, Material.RED_WOOL, Material.BLACK_WOOL
+        );
+
         if(enchantedGoldenAppleRecipe.asBoolean()) {
             initEnchantedGoldenAppleRecipe();
         }
@@ -41,6 +51,12 @@ public class OldRecipes extends VEModule {
             initChainmailChestplateRecipe();
             initChainmailLeggingsRecipe();
             initChainmailBootsRecipe();
+        }
+        if(horseArmorRecipes.asBoolean()) {
+            initLeatherHorseArmorRecipe();
+            initIronHorseArmorRecipe();
+            initGoldenHorseArmorRecipe();
+            initDiamondHorseArmorRecipe();
         }
     }
 
@@ -85,6 +101,38 @@ public class OldRecipes extends VEModule {
         ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(getPlugin(), "chainmail_boots"), new ItemStack(Material.CHAINMAIL_BOOTS));
         recipe.shape("F F", "F F");
         recipe.setIngredient('F', Material.FIRE);
+        Bukkit.addRecipe(recipe);
+    }
+
+    private void initLeatherHorseArmorRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(getPlugin(), "leather_horse_armor"), new ItemStack(Material.LEATHER_HORSE_ARMOR));
+        recipe.shape("  L", "LWL", "LLL");
+        recipe.setIngredient('L', Material.LEATHER);
+        recipe.setIngredient('W', woolChoice);
+        Bukkit.addRecipe(recipe);
+    }
+
+    private void initIronHorseArmorRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(getPlugin(), "iron_horse_armor"), new ItemStack(Material.IRON_HORSE_ARMOR));
+        recipe.shape("  L", "LWL", "LLL");
+        recipe.setIngredient('L', Material.IRON_INGOT);
+        recipe.setIngredient('W', woolChoice);
+        Bukkit.addRecipe(recipe);
+    }
+
+    private void initGoldenHorseArmorRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(getPlugin(), "gold_horse_armor"), new ItemStack(Material.GOLDEN_HORSE_ARMOR));
+        recipe.shape("  L", "LWL", "LLL");
+        recipe.setIngredient('L', Material.GOLD_INGOT);
+        recipe.setIngredient('W', woolChoice);
+        Bukkit.addRecipe(recipe);
+    }
+
+    private void initDiamondHorseArmorRecipe() {
+        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(getPlugin(), "diamond_horse_armor"), new ItemStack(Material.DIAMOND_HORSE_ARMOR));
+        recipe.shape("  L", "LWL", "LLL");
+        recipe.setIngredient('L', Material.DIAMOND);
+        recipe.setIngredient('W', woolChoice);
         Bukkit.addRecipe(recipe);
     }
 
