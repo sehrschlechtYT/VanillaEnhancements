c0b4e02859e468da91dd887467148d13b1db25e4
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2023-04-09 20:35:55 +0200

    Add 3rd party option for resource packs

--- src/main/java/yt/sehrschlecht/vanillaenhancements/items/resourcepack/ResourcePackManager.kt
+++ src/main/java/yt/sehrschlecht/vanillaenhancements/items/resourcepack/ResourcePackManager.kt
@@ -9,6 +9,7 @@ import yt.sehrschlecht.vanillaenhancements.VanillaEnhancements
 import yt.sehrschlecht.vanillaenhancements.modules.CustomTextureProvider
 import yt.sehrschlecht.vanillaenhancements.utils.debugging.Debug
 import java.io.File
+import java.net.URI
 import java.security.MessageDigest
 
 /**
@@ -37,22 +38,47 @@ class ResourcePackManager(val plugin: VanillaEnhancements) : Listener {
             plugin.logger.severe("Resource pack file is null! The resource pack server will not be started!")
             return
         }
-        plugin.logger.info("Starting resource pack server...")
-        server = runServer(packFile)
-        plugin.logger.info("Resource pack server started!")
 
+        if (plugin.config.getBoolean("resource_pack.3rd_party.enabled")) {
+            plugin.logger.info("Using 3rd party resource pack server.")
+            packUrl = plugin.config.getString("resource_pack.3rd_party.url") ?: ""
+            if (packUrl.isEmpty()) {
+                plugin.logger.severe("3rd party is enabled but no url is set in the config! (resource_pack.3rd_party.url)")
+                return
+            }
+            val providedHash = plugin.config.getString("resource_pack.3rd_party.hash") ?: ""
+            hash = if (providedHash.isEmpty()) {
+                calculateHash(packUrl)
+            } else {
+                providedHash.toByteArray()
+            }
+        } else {
+            plugin.logger.info("Starting resource pack server...")
+            server = runServer(packFile)
+            plugin.logger.info("Resource pack server started!")
+
+            val port = plugin.config.getInt("resource_pack.port")
+            val hostname = plugin.config.getString("resource_pack.hostname")
+            packUrl = "http://$hostname:$port/pack.zip" // ToDo support https
+            hash = calculateHash(packFile)
+        }
         plugin.server.pluginManager.registerEvents(this, plugin)
-        val port = plugin.config.getInt("resource_pack.port")
-        val hostname = plugin.config.getString("resource_pack.hostname")
-        packUrl = "http://$hostname:$port/pack.zip" // ToDo support https
-        hash = calculateHash(packFile)
     }
 
     private fun calculateHash(file: File): ByteArray {
-        // byte array size must be 20
-        // algorithm: SHA-1
-        val data: ByteArray = file.readBytes()
-        return MessageDigest.getInstance("SHA-1").digest(data)
+        with(file.readBytes()) {
+            return calculateHash(this)
+        }
+    }
+
+    private fun calculateHash(url: String): ByteArray {
+        with(URI(url).toURL().openStream().readBytes()) {
+            return calculateHash(this)
+        }
+    }
+
+    private fun calculateHash(bytes: ByteArray): ByteArray {
+        return MessageDigest.getInstance("SHA-1").digest(bytes)
     }
 
     private fun isEnabled(): Boolean {
