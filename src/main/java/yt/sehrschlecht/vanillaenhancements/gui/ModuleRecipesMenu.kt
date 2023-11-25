2bd9b39431a77a0725e61da49677845a047e16ee
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2023-11-25 16:45:42 +0100

    Add preview for shaped recipes

--- src/main/java/yt/sehrschlecht/vanillaenhancements/gui/ModuleRecipesMenu.kt
+++ src/main/java/yt/sehrschlecht/vanillaenhancements/gui/ModuleRecipesMenu.kt
@@ -4,9 +4,11 @@ import fr.minuskube.inv.ClickableItem
 import fr.minuskube.inv.SmartInventory
 import fr.minuskube.inv.content.InventoryContents
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ShapedRecipe
 import yt.sehrschlecht.vanillaenhancements.VanillaEnhancements
 import yt.sehrschlecht.vanillaenhancements.modules.ModuleTag
 import yt.sehrschlecht.vanillaenhancements.modules.RecipeModule
+import yt.sehrschlecht.vanillaenhancements.modules.VERecipe
 import yt.sehrschlecht.vanillaenhancements.utils.ItemCreator
 import yt.sehrschlecht.vanillaenhancements.utils.ModuleUtils
 import yt.sehrschlecht.vanillaenhancements.utils.SpigotExtensions.Companion.addBackButton
@@ -37,7 +39,12 @@ class ModuleRecipesMenu(private val plugin: VanillaEnhancements, private val mod
                 displayName("§f§l${ModuleUtils.getNameFromKey(it.key)}")
                 addLore("§fItem required for discovery: ${ModuleUtils.getNameFromKey(it.discoverItem?.name ?: "§cNone")}")
                 addLore(if (it.isRegistered) "§aRegistered" else "§cNot registered")
-            }.build()) { _ -> player.sendMessage("Clicked on recipe ${it.key}. TODO") } //ToDo
+                addLore(if (isRecipePreviewSupported(it)) "§9§oLeft click to preview" else "§c§oPreview isn't available yet for this recipe.")
+            }.build()) listener@{ _ ->
+                val gui = getPreviewGUIForRecipe(it)
+                gui?: return@listener
+                gui.open(player)
+            }
         }
 
         contents.paginateItems(items, player = player, noneItem = {
@@ -47,6 +54,15 @@ class ModuleRecipesMenu(private val plugin: VanillaEnhancements, private val mod
 
         contents.addBackButton{_ -> ModuleMenu.getInventory(plugin, module, sourceTag) }
         contents.fillBackground()
+    }
+
+    private fun isRecipePreviewSupported(recipe: VERecipe): Boolean {
+        return recipe.recipe is ShapedRecipe
+    }
+
+    private fun getPreviewGUIForRecipe(recipe: VERecipe): SmartInventory? {
+        if (recipe.recipe is ShapedRecipe) return ShapedRecipePreviewMenu.getInventory(plugin, recipe, getInventory(plugin, module, sourceTag))
+        return null
     }
 
 }
