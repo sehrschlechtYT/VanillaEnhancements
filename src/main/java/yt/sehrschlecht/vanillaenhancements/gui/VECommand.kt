cc26c33a135954ad9dad3e92295e6f1ec50ea323
sehrschlechtYT <66412605+sehrschlechtYT@users.noreply.github.com>
2024-02-15 22:54:05 +0100

    add argument for module key to /ve command

--- src/main/java/yt/sehrschlecht/vanillaenhancements/gui/VECommand.kt
+++ src/main/java/yt/sehrschlecht/vanillaenhancements/gui/VECommand.kt
@@ -1,5 +1,6 @@
 package yt.sehrschlecht.vanillaenhancements.gui
 
+import org.bukkit.NamespacedKey
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -20,18 +21,33 @@ class VECommand(val plugin: VanillaEnhancements) : CommandExecutor, TabExecutor 
             sender.sendMessage("§cYou must be a player to execute this command!")
             return true
         }
-        if (args?.size == 1) {
-            val arg = args[0]
-            val tag = plugin.moduleRegistry.collectTags().find { t -> t.key.equals(arg, true) }
+        if (args?.size == 1 || args?.size == 2) {
+            val moduleTagArg = args[0]
+            val tag = plugin.moduleRegistry.collectTags().find { t -> t.key.equals(moduleTagArg, true) }
             if (tag == null) {
                 sender.sendMessage("§cThe given module tag was not found!")
                 return true
             }
-            ModuleListMenu.getInventory(plugin, tag).open(sender)
+            if (args.size == 1) {
+                ModuleListMenu.getInventory(plugin, tag).open(sender)
+                return true
+            }
+            val moduleKeyArg = args[1]
+            val namespacedKey = NamespacedKey.fromString(moduleKeyArg)
+            if (namespacedKey == null) {
+                sender.sendMessage("§cThe given module key is not formatted correctly!")
+                return true
+            }
+            val module = plugin.moduleRegistry.getModule(namespacedKey)
+            if (module == null) {
+                sender.sendMessage("§cCould not find a module for the given module key!")
+                return true
+            }
+            ModuleMenu.getInventory(plugin, module, tag).open(sender)
             return true
         }
         if (args?.size != 0) {
-            sender.sendMessage("§cCommand usage: /ve [module tag]")
+            sender.sendMessage("§cCommand usage: /ve [module tag] [module key]")
             return true
         }
         MainMenu.getInventory(plugin).open(sender)
@@ -45,9 +61,15 @@ class VECommand(val plugin: VanillaEnhancements) : CommandExecutor, TabExecutor 
         args: Array<out String>?
     ): MutableList<String> {
         args?: return mutableListOf()
-        if (args.size != 1) return mutableListOf()
-        val tags = plugin.moduleRegistry.collectTags()
-        return CommandUtils.tabComplete(tags.map(ModuleTag::key), args[0])
+        if (args.isEmpty() || args.size > 2) return mutableListOf()
+        if (args.size == 1) {
+            val tags = plugin.moduleRegistry.collectTags()
+            return CommandUtils.tabComplete(tags.map(ModuleTag::key), args[0])
+        } else {
+            val tag = plugin.moduleRegistry.collectTags().find { t -> t.key.equals(args[0], true) } ?: return mutableListOf()
+            val moduleKeysForTag = plugin.moduleRegistry.getModulesByTag(tag).map { module -> module.moduleKey.toString() }
+            return CommandUtils.tabComplete(moduleKeysForTag, args[1])
+        }
     }
 
 }
